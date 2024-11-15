d8c107fae0eecae3eeaf4bf45ff067dc4db9311f
Jon Olav Eikenes <jonolav.eikenes@gmail.com>
2024-11-15 13:24:38 +0100

    one more

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/warp-chart-styling-poc/',
   build: {
     target: 'esnext' // Ensures support for Top-level await
   },
